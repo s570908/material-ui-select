98c97b5a5be445280f14e60892df7dfe0206ab2f
s570908 <30783468+s570908@users.noreply.github.com>
2022-09-14 11:44:59 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+import SimpleSelect from "./material.js";
+
+export default function App() {
+  return (
+    <SimpleSelect> </SimpleSelect>
+  );
+}
---
+++ src/material.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
+const lists = ["Ten", "twenty", "Thirty", "Fourty", "Fifity", "Sixty"];
+
+export default function SimpleSelect() {
+  const classes = useStyles();
+  const [age, setAge] = React.useState("Thirty");
+
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+
+  const handleChange = event => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel ref={inputLabel} id="demo-simple-select-outlined-label">
+          Age
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={age}
+          onChange={handleChange}
+          labelWidth={labelWidth}
+        >
+          {lists.map(item => (
+            <MenuItem value={item}>{item}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
